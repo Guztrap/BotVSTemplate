3ed232e81ac59056fd221eb6b73de5cf9dd05e99
Gustavo David Ramirez Ledesma <gramirez@netintellego.com>
2019-01-07 09:45:01 -0600

    Bot in VS

---
+++ Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            if (activity.Text.Contains("Assortment"))
+            {
+                await context.PostAsync($"Is an application developed for Ann Inc in order to create the seasons assort");
+            }
+
+            var length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BotsTemplate 
+Bot creado con el **Bot Template** sin conexion a nada, respuestas codificadas
